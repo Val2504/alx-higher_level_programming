629b9db3626238b0c5ed4ecb88861dd35c7fad05
Val2504 <uchechukwuvalentine47@gmail.com>
2024-05-07 14:14:57 +0000

    task 12

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,63 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "lists.h"
 
-/**
- * print_listint - prints all elements of a listint_t list
- * @h: pointer to head of list
- * Return: number of nodes
- */
-size_t print_listint(const listint_t *h)
-{
-    const listint_t *current;
-    unsigned int n; /* number of nodes */
+int check_cycle(listint_t *list) {
+    if (list == NULL)
+        return 0;  /* No cycle in an empty list */
 
-    current = h;
-    n = 0;
-    while (current != NULL)
-    {
-        printf("%i\n", current->n);
-        current = current->next;
-        n++;
+    listint_t *slow = list;  /* Slow pointer moves 1 step at a time */
+    listint_t *fast = list->next;  /* Fast pointer moves 2 steps at a time */
+
+    /* Traverse the list until slow pointer meets fast pointer or fast reaches end of list */
+    while (fast != NULL && fast->next != NULL) {
+        if (slow == fast) {
+            return 1;  /* Cycle detected */
+        }
+        slow = slow->next;
+        fast = fast->next->next;
     }
 
-    return (n);
-}
-
-/**
- * add_nodeint - adds a new node at the beginning of a listint_t list
- * @head: pointer to a pointer of the start of the list
- * @n: integer to be included in node
- * Return: address of the new element or NULL if it fails
- */
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-    listint_t *new;
-
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-        return (NULL);
-
-    new->n = n;
-    new->next = *head;
-    *head = new;
-
-    return (new);
-}
-
-/**
- * free_listint - frees a listint_t list
- * @head: pointer to list to be freed
- * Return: void
- */
-void free_listint(listint_t *head)
-{
-    listint_t *current;
-
-    while (head != NULL)
-    {
-        current = head;
-        head = head->next;
-        free(current);
-    }
+    return 0;  /* No cycle found */
 }
--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -4,21 +4,18 @@
 #include <stdlib.h>
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
+ * struct listint_s - ListNode
+ * @val: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
  *
  */
-typedef struct listint_s
-{
-    int n;
-    struct listint_s *next;
+typedef struct ListNode {
+    int val;
+    struct ListNode *next;
 } listint_t;
 
-size_t print_listint(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int n);
-void free_listint(listint_t *head);
+
 int check_cycle(listint_t *list);
 #endif

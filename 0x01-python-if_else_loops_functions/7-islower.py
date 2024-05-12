9386bb127772a355a437a79e47cf287158e0e171
Val2504 <uchechukwuvalentine47@gmail.com>
2024-05-12 15:30:15 +0000

    task 8

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def islower(c):
-    for char in c:
-        if ('a' <= char <= 'z'):
-            return True
-        return False
+    if ('a' <= char <= 'z'):
+        return True
+    return False

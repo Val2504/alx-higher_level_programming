f2cb355864ba07beef987139e414b474f911cfc9
Val2504 <uchechukwuvalentine47@gmail.com>
2024-05-15 14:24:52 +0000

    task 8

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def islower(c):
-        if (ord(a) <= ord(char) <= ord(z)):
-            return True
-        elif
-            return False
+    return (ord(a) <= ord(char) <= ord(z))

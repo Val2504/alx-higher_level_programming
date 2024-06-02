6056a98787baa0f98a94b7a214d21cf6369b7602
Val2504 <uchechukwuvalentine47@gmail.com>
2024-06-02 18:00:49 +0000

    task 10 adds two integers

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def add(a, b):
     if a > 0 and b > 0:
-        return (a + b)
+        result = a + b
     elif a < 0 and b > 0:
-        return (-a + b)
+        result = -a + b
     elif a > 0 and b < 0:
-        return (a + (-b))
+        result = a + (-b)
     else if a < 0 and b < 0:
-        return ((-a) + (-b))
+        result = (-a) + (-b)
+
+    return result

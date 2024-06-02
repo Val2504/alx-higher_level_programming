a9e7a62f3966b8a841531b943abba4f6af9f62b0
Val2504 <uchechukwuvalentine47@gmail.com>
2024-06-02 16:10:07 +0000

    corrected task

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last_digits = number % 10
-    print(last_digits)
-return last_digits
+    last_digit = number % 10
+    print(last_digit)
+    return last_digit

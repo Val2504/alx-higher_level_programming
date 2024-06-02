7e784d42ce21f8d64f2e4194f1fc5a3a3b8018af
Val2504 <uchechukwuvalentine47@gmail.com>
2024-06-02 17:27:45 +0000

    corrected task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,7 +2,7 @@
 def print_last_digit(number):
     last_digit = number % 10
     if last_name < 0:
-        return abs(last_digit)
+        print(abs(last_digit))
     else:
         print(last_digit, end='')
-        return last_digit
+    return last_digit

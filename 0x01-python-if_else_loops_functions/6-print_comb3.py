e3ea365f79ad6b18bfe79cb8a045be6e992314e3
Val2504 <uchechukwuvalentine47@gmail.com>
2024-06-03 10:59:50 +0000

    corrected task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 for val in range(0, 10):
     for num in range(val + 1, 10):
-        print("{}{}".format(val, num), end=' ')
-        if val != 9 and num != val - 1:
+        if val == 8 and num == 9:
+            print("{}{}".format(val, num))
+        else:
+            print("{}{}".format(val, num) end=', ')

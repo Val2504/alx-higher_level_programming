ca05048f41259f37fa1788fa879fe931872f1b20
Val2504 <uchechukwuvalentine47@gmail.com>
2024-06-04 12:22:42 +0000

    This is the first task on modules task

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,8 +4,5 @@ from add_0 import add
 a = 1
 b = 2
 
-def fake_add(a, b):
-    return a - b
-
 if __name__ == "__main__":
-    print("{} + {} = {}".format(a, b, fake_add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))

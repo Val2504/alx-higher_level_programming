e91e62e25f2e859f2d38232a41e546f11c858ff1
Val2504 <uchechukwuvalentine47@gmail.com>
2024-06-04 16:02:05 +0000

    corrected task

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,13 +5,12 @@ a = 10
 b = 5
 
 for_add = add(a, b)
-print("{} + {} = {}".format(a, b, for_add))
-
 for_sub = sub(a, b)
-print("{} - {} = {}".format(a, b, for_sub))
-
 for_mul = mul(a, b)
-print("{} * {} = {}".format(a, b, for_mul))
-
 for_div = int(div(a, b))
+
+if __name__ =="__main__"
+print("{} + {} = {}".format(a, b, for_add))
+print("{} - {} = {}".format(a, b, for_sub))
+print("{} * {} = {}".format(a, b, for_mul))
 print("{} / {} = {}".format(a, b, for_div))

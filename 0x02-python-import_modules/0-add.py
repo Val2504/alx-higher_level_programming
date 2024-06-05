3773de8882fc05974d96753a079b865802922f72
Val2504 <uchechukwuvalentine47@gmail.com>
2024-06-05 09:30:04 +0000

    corrected task 1

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-add_module = __import__("add_0")
+if __name__ ="__main__":
+    from add_0 import add
 
-add_function = add_module.add
+    a = 1
+    b = 2
 
-a = 1
-b = 2
+    result = add(a, b)
 
-result = add_function(a, b)
-
-print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, result))

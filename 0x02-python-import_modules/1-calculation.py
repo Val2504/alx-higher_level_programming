ce2782c9ebb3fbed1e593fae808902616372c43d
Val2504 <uchechukwuvalentine47@gmail.com>
2024-06-06 15:19:05 +0000

    task 2 imports some strings, and make sure that it doesn't run when imported but only when run directly

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
 
-a = 10
-b = 5
+    a = 10
+    b = 5
 
-for_add = add(a, b)
-for_sub = sub(a, b)
-for_mul = mul(a, b)
-for_div = int(div(a, b))
+    for_add = add(a, b)
+    for_sub = sub(a, b)
+    for_mul = mul(a, b)
+    for_div = int(div(a, b))
 
-if __name__ =="__main__"
-print("{} + {} = {}".format(a, b, for_add))
-print("{} - {} = {}".format(a, b, for_sub))
-print("{} * {} = {}".format(a, b, for_mul))
-print("{} / {} = {}".format(a, b, for_div))
+    print("{} + {} = {}".format(a, b, for_add))
+    print("{} - {} = {}".format(a, b, for_sub))
+    print("{} * {} = {}".format(a, b, for_mul))
+    print("{} / {} = {}".format(a, b, for_div))
